086e1f1828074bd7be2d00541d438ba7ffc3db56
igranet <igranet@wustl.edu>
2022-11-30 15:58:00 -0500

    Resize header

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,10 @@ const Header = () => {
 		<div className="md:w-full flex w-full fixed md:grid md:grid-cols-2 px-2 pt-4 bg-chill-bg z-50">
 			<div className="w-full flex">
 				<span className="font-medium italic text-sky-600 my-auto">
-					<img src={chillLogo} className="h-6 md:ml-24 ml-4" />
+					<img src={chillLogo} className="h-6 md:pl-24 ml-4" />
 				</span>
 			</div>
-			<div className="w-full flex flex-row justify-end px-24 space-x-4 text-sm md:block hidden">
+			<div className="w-3/4 flex-row justify-end mr-24 space-x-4 text-sm md:flex hidden">
 				<button className="my-auto text-gray-700 hover:bg-sky-500 p-2 rounded-md duration-200 cursor-pointer">
 					About
 				</button>
