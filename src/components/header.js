364a18a70114fedd168646ff4b75db2d3f3b768a
igranet <igranet@wustl.edu>
2022-11-30 15:54:16 -0500

    header sty

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,13 @@ const Header = () => {
 	const headerLinks = [];
 
 	return (
-		<div className="w-full fixed grid grid-cols-2 px-2 pt-4 bg-chill-bg">
+		<div className="md:w-full flex w-full fixed md:grid md:grid-cols-2 px-2 pt-4 bg-chill-bg z-50">
 			<div className="w-full flex">
 				<span className="font-medium italic text-sky-600 my-auto">
-					<img src={chillLogo} className="h-6 ml-24" />
+					<img src={chillLogo} className="h-6 md:ml-24 ml-4" />
 				</span>
 			</div>
-			<div className="w-full flex flex-row justify-end px-24 space-x-4 text-sm">
+			<div className="w-full flex flex-row justify-end px-24 space-x-4 text-sm md:block hidden">
 				<button className="my-auto text-gray-700 hover:bg-sky-500 p-2 rounded-md duration-200 cursor-pointer">
 					About
 				</button>
